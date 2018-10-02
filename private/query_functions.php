818898c74e82a11eeb8066cdeab0769526a1b225
joaquin-siri <joaquinsiri@gmail.com>
2018-10-02 03:13:40 -0300

    Repaso CRUD php

--- private/query_functions.php
+++ private/query_functions.php
@@ -2,26 +2,83 @@
 
 
 function find_all_subjects(){
-  global $db;
+    global $db;
 
-$sql = "SELECT * FROM subjects ";
-$sql .= "ORDER BY position ASC";
-$result = mysqli_query($db, $sql);
-confirm_result_set($result);
-return $result;
+  $sql = "SELECT * FROM subjects ";
+  $sql .= "ORDER BY position ASC";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  return $result;
 
 }
+
 function find_all_pages(){
   global $db;
 
-$sql = "SELECT * FROM pages ";
-$sql .= "ORDER BY subject_id ASC, position ASC";
-$result = mysqli_query($db, $sql);
-confirm_result_set($result);
-return $result;
+  $sql = "SELECT * FROM pages ";
+  $sql .= "ORDER BY subject_id ASC, position ASC";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  return $result;
 
 }
 
+function find_subject_by_id($id) {
+    global $db;
+
+  $sql = "SELECT * FROM subjects ";
+  $sql .= "WHERE id='" . $id . "'";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  $subject = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  return $subject; //return an assoc. array
+}
+
+function update_subject($subject){
+  global $db;
+
+  $sql ="UPDATE subjects SET ";
+  $sql .="menu_name='" . $subject ['menu_name']. "', ";
+  $sql .="position='" . $subject ['position']. "', ";
+  $sql .="visible='" . $subject ['visible']. "' ";
+  $sql .="WHERE id ='" . $subject ['id'] . "' ";
+  $sql .="LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+  //For UPDATE statements the result is true/false
+
+  if ($result){
+    return true;
+  } else {
+    //UPDATE failed
+    echo mysqli_error($db);
+    db_disconnect($db);
+    exit;
+  }
+}
+
+function insert_subject($subject){
+  global $db;
+
+  $sql = "INSERT INTO subjects ";
+  $sql .= "(menu_name, position, visible) ";
+  $sql .= "VALUES (";
+  $sql .= "'" . $subject['menu_name'] . "',";
+  $sql .= "'" . $subject['position'] . "',";
+  $sql .= "'" . $subject['visible'] . "')";
+  $result = mysqli_query($db, $sql);
+  //For INSERT statements, results are true/false
+  if($result){
+    return true;
+
+  } else {
+    //INSERT failed
+     echo mysqli_error($db);
+     db_disconnect($db);
+     exit;
+  }
+}
 
 
  ?>
